1f2ed6f606c8235d2f249b919ec18fb5ff019405
Onur Senture <woof.uyelik@gmail.com>
2013-12-27 13:43:55 +0200

    diliysins

--- wp-content/plugins/wp-i18n/menu/menu-footer.php
+++
@@ -1,4 +0,0 @@
-<div class="updated message">
-<h3 style="margin-bottom:0px;"><?php echo __('Need help?', 'sitepress')?></h3>
-<p style="line-height:1.4em;"><?php printf(__("Please post in the WordPress.org forums for this plugin.", 'sitepress')); ?></p>
-</div>
